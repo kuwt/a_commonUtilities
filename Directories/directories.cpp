067d73dcd2e432e39a95e12aa9c84633e9dcd2dd
kuwingto <kuwingto@gmail.com>
2018-08-20 16:30:20 +0800

    bugfix and add function

--- Directories/directories.cpp
+++ Directories/directories.cpp
@@ -1,68 +1,102 @@
 #include <experimental/filesystem>
 #include <string>
 #include <vector>
+#include <iostream>
 
 namespace fs = std::experimental::filesystem;
 
-// return the filenames of all files that have the specified extension
-// in the specified directory and all subdirectories
-void get_all_file(const fs::path& root,
-	const std::string& ext,
-	std::vector<fs::path>& ret)
+namespace dir
 {
-	if (!fs::exists(root) || !fs::is_directory(root)) return;
-
-	fs::recursive_directory_iterator it(root);
-	fs::recursive_directory_iterator endit;
-
-	while (it != endit)
+	// return the filenames of all files that have the specified extension
+	// in the specified directory and all subdirectories
+	void get_all_file(const fs::path& root,
+		const std::string& ext,
+		std::vector<fs::path>& ret)
 	{
-		if (fs::is_regular_file(*it) && it->path().extension() == ext)
+		if (!fs::exists(root) || !fs::is_directory(root)) return;
+
+		fs::recursive_directory_iterator it(root);
+		fs::recursive_directory_iterator endit;
+
+		while (it != endit)
 		{
-			ret.push_back(it->path());
+			if (fs::is_regular_file(*it) && it->path().extension() == ext)
+			{
+				ret.push_back(it->path());
+			}
+			++it;
 		}
-		++it;
 	}
-}
 
 
-int listAllFilesPathsFromDir(const std::string& dirPath, 
-	const std::string &extension, 
-	std::vector<std::string> &path)
-{
-	path.clear();
-	std::vector<fs::path> dataPaths;
-
+	int listAllFilesPathsFromDir(const std::string& dirPath,
+		const std::string &extension,
+		std::vector<std::string> &path)
 	{
-		std::vector<fs::path> dataPathsPerExt;
+		path.clear();
+		std::vector<fs::path> dataPaths;
 
-		dataPathsPerExt.clear();
-		get_all_file(dirPath,
-			extension,
-			dataPaths);
-		dataPaths.insert(dataPaths.end(), dataPathsPerExt.begin(), dataPathsPerExt.end());
+		{
+			std::vector<fs::path> dataPathsPerExt;
 
+			dataPathsPerExt.clear();
+			get_all_file(dirPath,
+				extension,
+				dataPaths);
+			dataPaths.insert(dataPaths.end(), dataPathsPerExt.begin(), dataPathsPerExt.end());
+
+		}
+		for (int i = 0; i < dataPaths.size(); ++i)
+		{
+			std::string dataPath = dataPaths.at(i).string();
+			path.push_back(dataPath);
+		}
+
+		return 0;
 	}
-	for (int i = 0; i < dataPaths.size(); ++i)
+
+	bool CheckIfFilePathContainSubPath(const std::string& FilePath,
+		const std::string &SubPath)
 	{
-		std::string dataPath = dataPaths.at(i).string();
-		path.push_back(dataPath);
+		int wildcard_pos = FilePath.find(SubPath);
+		if (wildcard_pos != std::string::npos)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
 	}
 
-	return 0;
-}
-
-bool CheckIfFilePathContainSubPath(const std::string& FilePath,
-	const std::string &SubPath)
-{
-	int wildcard_pos = FilePath.find(SubPath);
-	if (wildcard_pos != std::string::npos)
+	void CreateDir(const std::string &dir)
 	{
-		return true;
-	}
-	else
-	{
-		return false;
+		if (fs::create_directories(dir))
+		{
+			std::cerr << "Directory Created: " << dir << std::endl;
+		}
 	}
 
+	void MakeDefaultDir(const std::string &Rootdir, const std::string &folderName, std::string &Defaultdir)
+	{
+		int proposeDirIdx = 0;
+		std::string strproposeDirIdx = std::to_string(proposeDirIdx);
+		std::string proposeDir = Rootdir + "\\" + folderName;
+
+		std::string proposeFullDir = proposeDir + strproposeDirIdx;
+
+		while (fs::exists(proposeFullDir))
+		{
+			proposeDirIdx++;
+			strproposeDirIdx = std::to_string(proposeDirIdx);
+
+			proposeFullDir = proposeDir + strproposeDirIdx;
+		}
+
+		std::string NewDir = proposeFullDir;
+		CreateDir(NewDir);
+
+		Defaultdir = NewDir;
+	}
 }
--- Directories/directories.h
+++ Directories/directories.h
@@ -3,10 +3,16 @@
 #include <string>
 #include <vector>
 
+namespace dir
+{
+	int listAllFilesPathsFromDir(const std::string& dirPath,
+		const std::string &extension,
+		std::vector<std::string> &path);
 
-int listAllFilesPathsFromDir(const std::string& dirPath,
-	const std::string &extension,
-	std::vector<std::string> &path);
+	bool CheckIfFilePathContainSubPath(const std::string& FilePath,
+		const std::string &SubPath);
 
-bool CheckIfFilePathContainSubPath(const std::string& FilePath,
-	const std::string &SubPath);
+	void CreateDir(const std::string &dir);
+
+	void MakeDefaultDir(const std::string &Rootdir, const std::string &folderName, std::string &Defaultdir);
+}
